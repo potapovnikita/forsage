5c9b753272a156a75db577f77ce4e1df75e2e4b4
potapov.nikita <potapov.nikita@itmh.ru>
2019-01-21 03:05:19 +0500

    gulp change

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ gulp.task('deploy:production', function () {
     return gulp.src('dist/**/*')
         .pipe(sftp({
             host: '31.31.196.165',
-            // keyContents: '' ,// process.env.SSH_KEY,
             user: 'u0635367',
             pass: 'UoBU4yG_',
             remotePath: '/www/forsagedanceschool.ru'
