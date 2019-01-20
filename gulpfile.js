734968cc6b64285d57f84c23db0965c331c27e16
potapov.nikita <potapov.nikita@itmh.ru>
2019-01-21 02:18:04 +0500

    drone ci

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ const sftp = require('gulp-sftp');
 gulp.task('deploy:production', function () {
     return gulp.src('dist/**/*')
         .pipe(sftp({
-            host: 'forsagedanceschool.ru',
-            keyContents: process.env.SSH_KEY,
-            remotePath: '/var/www/html',
-            user: 'root',
-            remotePlatform: 'unix',
+            host: '31.31.196.165',
+            // keyContents: '' ,// process.env.SSH_KEY,
+            user: 'u0635367',
+            pass: 'UoBU4yG_',
+            remotePath: '/www/forsagedanceschool.ru/'
         }));
 });
