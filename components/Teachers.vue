dbc0242c0a1eb4ece26e59a822d2965501e7989b
IVAN KELIK <47008744+kelikivan@users.noreply.github.com>
2020-08-20 00:42:07 +0500

    Update Teachers.vue

--- components/Teachers.vue
+++ components/Teachers.vue
@@ -70,7 +70,7 @@
                 const heightSlider = document.getElementById('active').clientHeight
                 const indexRow = this.rowVol - Math.ceil((index + 1) / 4)
                 const heightSliderConst =  this.rowVol > 1 ? heightSlider : 0
-                activeSlider.transform = `translate(${offsetLeft}px, ${-(heightPhoto + heightSlider) * indexRow /*- heightSliderConst*/}px)`
+                activeSlider.transform = `translate(${offsetLeft}px, ${-(heightPhoto + heightSlider) * indexRow}px)`
                 setTimeout(() => {
                     activeSlider.opacity = '1'
                 }, 300)
@@ -173,7 +173,7 @@
                 line-height normal
                 letter-spacing normal
                 position relative
-                top -15px
+                top 0px
 
                 .name
                     display flex
