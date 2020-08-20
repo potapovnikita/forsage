233421d97fa4ba9d9874c727d333e670f475acc0
IVAN KELIK <47008744+kelikivan@users.noreply.github.com>
2020-08-20 23:38:57 +0500

    Update Styles.vue

--- components/Styles.vue
+++ components/Styles.vue
@@ -15,10 +15,6 @@
                         frameborder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                         allowfullscreen)
-
-
-
-
 </template>
 
 <script>
@@ -58,7 +54,7 @@
 <style lang="stylus">
     .styles_inner-container
         background-color #000
-        background-image url('~assets/img/background-styles.png')
+        background-image url('~assets/img/backgrounds/styles.png')
         background-position 50%
         background-repeat no-repeat
         background-size cover
@@ -131,10 +127,10 @@
                 height 100%
     @media only screen and (max-width 1500px)
         .styles_inner-container
-            background-image url('~assets/img/background-styles.png')
+            background-image url('~assets/img/backgrounds/styles.png')
     @media only screen and (max-width 1000px)
         .styles_inner-container
-            background-image url('~assets/img/background-styles.png')
+            background-image url('~assets/img/backgrounds/styles.png')
         .styles_list
             margin 0 auto
             max-width 400px
@@ -147,7 +143,7 @@
 
     @media only screen and (max-width 450px)
         .styles_inner-container
-            background-image url('~assets/img/background-styles-min.png')
+            background-image url('~assets/img/backgrounds/styles-min.png')
             .styles-top
                 .title-text
                     padding-top 0
@@ -162,6 +158,4 @@
             div
                 width 100%
                 padding-bottom 48%
-
-
 </style>
