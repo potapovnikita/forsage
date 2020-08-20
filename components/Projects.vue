928d795fb546c8f7404ae60ce88316030188b3a7
IVAN KELIK <47008744+kelikivan@users.noreply.github.com>
2020-08-20 22:34:06 +0500

    Create Projects.vue

---
+++ components/Projects.vue
@@ -0,0 +1,167 @@
+<template lang="pug">
+    .styles_container
+        .styles_inner-container
+            .styles-top
+                h1.title.title-text Направления
+                    .line
+                .styles_list
+                    .styles_list-item(
+                        v-for="(item, index) in styles",
+                        :class="{active: index === activeStyle}",
+                        @click="selectStyle(index)") {{item.StyleName}}
+            .styles_video
+                div
+                    iframe(:src="videoUrl"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen)
+
+
+
+
+</template>
+
+<script>
+    import Data from '~/assets/staticData/styles.json'
+
+    export default {
+        data() {
+            return {
+                styles: Data.Styles,
+                activeStyle: 0,
+            }
+        },
+        components: {
+
+        },
+        methods: {
+            // выбор стиля
+            selectStyle(index) {
+                this.activeStyle = index
+            },
+        },
+        computed: {
+            videoUrl() {
+                return this.styles[this.activeStyle].YouTubeLink
+            }
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+    }
+
+</script>
+
+<style lang="stylus">
+    .styles_inner-container
+        background-color #000
+        background-image url('~assets/img/background-styles.png')
+        background-position 50%
+        background-repeat no-repeat
+        background-size cover
+        padding $PaddingContainers
+        padding-bottom 80px
+
+        .styles-top
+            margin-bottom 40px
+            .title-text
+                max-width 430px
+                padding-top 35px
+                padding-bottom 50px
+                position relative
+                margin 0 auto
+
+                .line
+                    width 118px
+                    height 5px
+                    position absolute
+                    background orangeMain
+                    top 35px
+                    right 0
+    .styles_list
+        display flex
+        justify-content center
+
+        &-item
+            color whiteMain
+            cursor pointer
+            font-family $FuturaFont
+            font-size $FontSizeMenu
+            font-weight bold
+            font-style normal
+            font-stretch normal
+            line-height normal
+            letter-spacing 0.3px
+            opacity 0.5
+            margin 0 30px 15px 0
+            white-space nowrap
+            border-bottom 2px solid
+            border-color rgba(0, 0, 0, 0)
+
+
+            &:hover
+                opacity 1
+
+            &:last-child
+                margin-right 0
+
+            &:first-child
+                margin-left 0
+        .active
+            border-bottom 2px solid orangeMain
+            opacity 1
+
+    .styles_video
+        div
+            position relative
+            margin 0 auto
+            width 60%
+            padding-bottom 32%
+            padding-top 25px
+            height 0
+
+            iframe
+                position absolute
+                top 0
+                left 0
+                width 100%
+                height 100%
+    @media only screen and (max-width 1500px)
+        .styles_inner-container
+            background-image url('~assets/img/background-styles.png')
+    @media only screen and (max-width 1000px)
+        .styles_inner-container
+            background-image url('~assets/img/background-styles.png')
+        .styles_list
+            margin 0 auto
+            max-width 400px
+            flex-wrap wrap
+
+    @media only screen and (max-width 767px)
+        .styles_inner-container
+            width $ContainersWidthMobile
+            padding $PaddingContainersMobile
+
+    @media only screen and (max-width 450px)
+        .styles_inner-container
+            background-image url('~assets/img/background-styles-min.png')
+            .styles-top
+                .title-text
+                    padding-top 0
+
+                    .line
+                        top 0
+
+        .styles_list-item
+            margin-right 15px
+
+        .styles_video
+            div
+                width 100%
+                padding-bottom 48%
+
+
+</style>
